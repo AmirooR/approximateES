7905d272493131b3bde1f61767b15cfd456d3f49
AmirooR <noname01.cpp@gmail.com>
2015-04-24 16:37:18 +0200

    cerr removed

--- EnergyMinimizer.hpp
+++ EnergyMinimizer.hpp
@@ -8,39 +8,4 @@ class EnergyMinimizer
    virtual short_array minimize(short_array input, float lambda, float& energy, float &m, float& b) = 0;
 };
 
-class TestEnergyMinimizer: public EnergyMinimizer
-{
-    //size_t N;
-    public:
-    
-    TestEnergyMinimizer()
-    {
-    }
-    
-    virtual short_array minimize(short_array input, float lambda, float& energy, float &m, float& b)
-    {
-        short_array x( new Short[1] );
-        if( lambda <= -0.5 )
-        {
-            m = 1;
-            b = 1;
-            energy = m*lambda + b;
-            x[0] = Short(0);
-        }
-        else if( lambda <= 0.5)
-        {
-            m = 0;
-            b = 0.5;
-            energy = m*lambda + b;
-            x[0] = Short(1);
-        }
-        else if( lambda <= 1)
-        {
-            m = -1;
-            b = 1;
-            energy = m*lambda + b;
-            x[0] = Short(2);
-        }
-        return x;
-    }
-};
+
